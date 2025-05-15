feda313289cc1a2f18612713ef81208fe14d5b46
SaartajAlam <107008756+SaartajAlam@users.noreply.github.com>
2025-05-15 12:14:18 -0400

    Delete ClosestPoint.java

--- ClosestPoint.java
+++
@@ -1,9 +0,0 @@
-public class ClosestPoint {
-    public Patient patient;
-    public float distance;
-
-    public ClosestPoint(Patient patient, float distance) {
-        this.patient = patient;
-        this.distance = distance;
-    }
-}
